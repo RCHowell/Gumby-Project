a4628aadfc5ac5724c6b248d4d030dea0dff31c6
R Conner Howell <rhowell2@nd.edu>
2019-01-31 11:07:17 -0500

    Version 1.0.3. Fix path to MainActivity.java

---
+++ android/app/src/main/java/com/rchowell/gumbyproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rchowell.gumbyproject;
+
+import android.os.Bundle;
+
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        GeneratedPluginRegistrant.registerWith(this);
+    }
+}
